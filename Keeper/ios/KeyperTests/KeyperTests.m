54721f5b3635c58b3a6457f37dd0fc24b45c36e7
Rodolfo Agustín Silav Messano <rodoagussm@gmail.com>
2023-05-13 12:05:30 -0300

    - App rename

--- Keeper/ios/KeyperTests/KeyperTests.m
+++ Keeper/ios/KeyperTests/KeyperTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface KeeperTests : XCTestCase
+@interface KeyperTests : XCTestCase
 
 @end
 
-@implementation KeeperTests
+@implementation KeyperTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
